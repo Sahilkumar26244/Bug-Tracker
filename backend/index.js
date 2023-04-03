3ab1e30bb35160f7c7f82974bfa5a2000ea129b3
Sahil Kumar <kumarsahil8981@gmail.com>
2023-04-03 12:23:37 +0530

    signup and login done

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,22 @@ const express = require("express");
 require('dotenv').config();
 const connectDB = require('./config/db');
 const cors = require('cors');
+const userRoutes = require('./routes/userRoutes');
 
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 connectDB();
 
 app.get('/',(req,res) => {
     res.send("welcome")
 });
+
+app.use('/users',userRoutes);
+
 
 const PORT = process.env.PORT || 4040
 app.listen(PORT,() => {
